dc4f6057702e2b94df01a1de05ed9b61edcc9f19
rodrigopavezi <rodrigopavezi@gmail.com>
2018-07-11 20:37:55 -0300

    fix: export components

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
-export { CryptoPriceBox }  from './price/crypto-price-box'
-export { NumberFormat }  from './price/number-format'
+export { CryptoPriceBox }  from './price/crypto-price-box';
+export { NumberFormat }  from './price/number-format';
+export { PriceSummary }  from './price/price-summary';
+export { TokenBox }  from './price/token-box';
+export { TokenPrice }  from './price/token-price';
+export { CustomTokenText }  from './price/custom-token-text';
 export { H1 } from "./typography/headings";
 export { P} from "./typography/paragraph";
