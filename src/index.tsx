767a82e88aae4971bc62ed93f6b8f4655fde90fd
rodrigopavezi <rodrigopavezi@gmail.com>
2018-07-12 14:49:33 -0300

    fix: export icons

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,6 @@ export { TokenPrice }  from './price/token-price';
 export { CustomTokenText }  from './price/custom-token-text';
 export { H1 } from "./typography/headings";
 export { P} from "./typography/paragraph";
+export { EthereumIcon } from './icons/ethereum';
+export { SelfkeyIcon } from './icons/selfkey';
+export { CustomIcon } from './icons/custom';
