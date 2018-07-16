effead19ea7cb1baf45890f67b0c4a3d46698226
Dan Cowell <admin@dfcowell.net>
2018-07-16 20:49:25 +1000

    refactor(copy): add typings to copy component

--- src/price/copy.tsx
+++ src/price/copy.tsx
@@ -1,66 +1,70 @@
-import * as React from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import injectSheet from 'react-jss';
-import { StyleSheet} from '../react-jss.types';
+import * as React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import injectSheet from "react-jss";
+import { StyleSheet, StyledComponentProps } from "../react-jss.types";
 
 const styles: StyleSheet = {
   copyButton: {
-    cursor: 'pointer',
-    width: '46px',
-    height: '20px',
-    marginLeft: '7px',
+    cursor: "pointer",
+    width: "46px",
+    height: "20px",
+    marginLeft: "7px",
     padding: 0,
-    backgroundColor: '#435160',
-    border: 'none',
-    borderRadius: '4px',
+    backgroundColor: "#435160",
+    border: "none",
+    borderRadius: "4px",
     margin: 0,
-    color: '#ffffff !important'
+    color: "#ffffff !important"
   },
 
   copyButtonSpan: {
-    fontSize: '11px',
-    lineHeight: '12px',
-    fontFamily: 'ProximaNovaRegular',
-    letterSpacing: 'normal',
-    color: '#93b0c1'
-  },
+    fontSize: "11px",
+    lineHeight: "12px",
+    fontFamily: "ProximaNovaRegular",
+    letterSpacing: "normal",
+    color: "#93b0c1"
+  }
 };
 
-export interface Props {
+export type CopyProps = StyledComponentProps & {
   text: string;
-}
+};
+
+export type CopyState = {
+  copyTextPlaceholder: string;
+};
 
 @injectSheet(styles)
-export class Copy extends React.Component<any, any> {
-  
-  copyText = 'COPY';
-  copiedText = 'COPIED';
+export class Copy extends React.Component<CopyProps, CopyState> {
+  copyText = "COPY";
+  copiedText = "COPIED";
 
   state = {
     copyTextPlaceholder: this.copyText
   };
 
-  constructor(props: any) {
+  constructor(props: CopyProps) {
     super(props);
 
     this.handleOnCopy = this.handleOnCopy.bind(this);
   }
 
   handleOnCopy() {
-    this.setState({copyTextPlaceholder: this.copiedText});
-    const bounceTime = setTimeout( () => {
-      this.setState({copyTextPlaceholder: this.copyText});
+    this.setState({ copyTextPlaceholder: this.copiedText });
+    const bounceTime = setTimeout(() => {
+      this.setState({ copyTextPlaceholder: this.copyText });
       clearTimeout(bounceTime);
-    }, 1000)
-  };
+    }, 1000);
+  }
 
   render() {
-    const {text, classes} = this.props
+    const { text, classes } = this.props;
     return (
-      <CopyToClipboard text={text}
-          onCopy={this.handleOnCopy}>
+      <CopyToClipboard text={text} onCopy={this.handleOnCopy}>
         <button className={classes.copyButton}>
-          <span className={classes.copyButtonSpan}>{this.state.copyTextPlaceholder}</span>
+          <span className={classes.copyButtonSpan}>
+            {this.state.copyTextPlaceholder}
+          </span>
         </button>
       </CopyToClipboard>
     );
