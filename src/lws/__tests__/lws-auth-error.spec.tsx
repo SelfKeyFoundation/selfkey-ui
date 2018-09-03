050e0cde154df0ed3c4f5d8581915423806048a2
Maxim Kovalov <max.kovalov@gmail.com>
2018-09-03 19:55:36 +0300

    test(lws): add tests to new components

---
+++ src/lws/__tests__/lws-auth-error.spec.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { create } from 'react-test-renderer';
+import { LWSAuthError } from '../lws-auth-error';
+
+describe('LWS Auth Error Screen', () => {
+	it('should show LWS Auth Error Screen', () => {
+		const component = create(
+			<LWSAuthError
+				website={{
+					name: 'PaveziCoin',
+					url: 'http://www.pavezicoin.org',
+					termsUrl: 'http://www.pavezicoin.org/terms',
+					policyUrl: 'http://www.pavezicoin.org/privacy',
+				}}
+			/>
+		);
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
