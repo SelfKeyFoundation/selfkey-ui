18359e45b2a1888b1036ef72301fb44bacd18391
rodrigopavezi <rodrigopavezi@gmail.com>
2018-09-07 08:12:01 +0200

    fix: view action

--- src/marketplace/items/item.tsx
+++ src/marketplace/items/item.tsx
@@ -63,7 +63,7 @@ export type ItemProps = {
   status: string,
   description: string,
   logoUrl?: string,
-  viewAction?: ((event: React.MouseEvent<HTMLElement>) => void)
+  viewAction?: Function
 }
 
 
