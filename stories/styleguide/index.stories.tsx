e8ee399c4208fd4e84a893fa0a9ee6ecefce3ed6
rodrigopavezi <rodrigopavezi@gmail.com>
2018-09-28 17:15:45 +0100

    fix: merge

--- stories/styleguide/index.stories.tsx
+++ stories/styleguide/index.stories.tsx
@@ -18,8 +18,6 @@ import { TransactionSendProgressBox } from '../../src/transaction/send/transacti
 import { LWSModalHeader } from '../../src/lws/lws-modal-header';
 import { LWSLoading } from '../../src/lws/lws-loading';
 import { TransactionHistory, StatusIconName } from '../../src/transaction/transaction-history';
-import { LWSSelectWallet } from '../../src/lws/lws-select-wallet';
-import { LWSSelectWalletModal, LWSRequiredInfoModal, LWSWalletConnectionErrorModal, LWSSelfkeyIdErrorModal, LWSExtensionErrorModal, LWSSuccessrModal } from './lws';
 import { SendTransaction } from './send-transaction';
 
 import {
@@ -194,16 +192,13 @@ lws.addWithJSX('LWSSelectWallet', () => <LWSSelectWalletWrapper />);
 lws.addWithJSX('LWSRequiredInfo', () => <LWSRequiredInfoWrapper />);
 
 lws.addWithJSX('LWSWalletConnectionError', () => <LWSWalletConnectionErrorWrapper />);
-lws.addWithJSX('LWSSelectWalletModal', () => <LWSSelectWalletModal />);
 
 lws.addWithJSX('LWSSelfkeyIdError', () => <LWSSelfkeyIdErrorWrapper />);
 
 lws.addWithJSX('LWSExtensionError', () => <LWSExtensionErrorWrapper />);
 
 lws.addWithJSX('LWSSuccess', () => <LWSSuccessWrapper />);
-lws.addWithJSX('LWSWalletConnectionErrorModal', () => <LWSWalletConnectionErrorModal />);
-
-lws.addWithJSX('LWSSelfkeyIdErrorModal', () => <LWSSelfkeyIdErrorModal />);
+lws.addWithJSX('LWSWalletConnectionErrorModal', () => <LWSWalletConnectionErrorWrapper />);
 
 lws.addWithJSX('LWSAuthError', () => <LWSAuthErrorWrapper />);
 
@@ -240,4 +235,3 @@ marketplaceStory.addWithJSX('ItemDetails', () => (
 		}}
 	/>
 ));
-lws.addWithJSX('LWSSuccessrModal', () => <LWSSuccessrModal />);
