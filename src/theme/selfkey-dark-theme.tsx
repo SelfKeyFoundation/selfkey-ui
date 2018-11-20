60ea57067619744492349d1544ce6721dc18369b
designhorf <designhorf@gmail.com>
2018-11-20 22:28:34 +0100

    Typography: defined headings, body texts, tables and forms

--- src/theme/selfkey-dark-theme.tsx
+++ src/theme/selfkey-dark-theme.tsx
@@ -3,13 +3,17 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme({
     palette: {
-      primary: { main: '#00C0D9' }, 
+      primary: { 
+        main: '#00C0D9',
+        light: '#23E6FE',
+      }, 
       secondary: { main: '#697C95' },
+      error: { main: '#FE4B61' },
       text: {
         primary: "#FFFFFF",
-        secondary: "rgba(0, 0, 0, 0.54)",
-        disabled: "rgba(0, 0, 0, 0.38)",
-        hint: "rgba(0, 0, 0, 0.38)"
+        secondary: "#93B0C1",
+        disabled: "#697C95",
+        hint: "rgba(0, 0, 0, 0.38)",
       }
     },
     typography: {
@@ -19,7 +23,7 @@ const theme = createMuiTheme({
           'sans-serif'
         ].join(','),
         fontSize: 16,
-        useNextVariants: true
+        useNextVariants: true,
     },
     overrides: {
       MuiCard: { 
@@ -27,9 +31,48 @@ const theme = createMuiTheme({
           backgroundColor: '#262F39',
         },
       },
+      MuiTypography: {
+        root: {
+          color: '#BE1E1E',
+        },
+        h1: {
+          fontSize: '24px',
+          fontWeight: 400,
+          lineHeight: '30px',
+        },
+        h2: {
+          fontSize: '18px',
+          fontWeight: 400,
+          lineHeight: '26px',
+        },
+        h3: {
+          fontSize: '16px',
+          lineHeight: '24px',
+        },
+        body1: {
+          fontSize: '18px',
+          lineHeight: '30px',
+        },
+        body2: {
+          fontSize: '16px',
+          lineHeight: '24px',
+        },
+        subtitle1: {
+          fontSize: '14px',
+          lineHeight: '17px',
+        },
+        subtitle2: {
+          fontSize: '12px',
+          lineHeight: '15px',
+        },
+        caption: {
+          fontSize: '13px',
+          lineHeight: '19px',
+        },
+      },
     },
-    
 });
+
 
 export const SelfkeyDarkTheme: React.SFC<{}> = ({children}) => {
     return (
