8cbfc60122c39c8ce61aca4b2c20f3377f986b9a
designhorf <designhorf@gmail.com>
2018-11-29 10:31:21 +0100

    defined own styled edit icon

---
+++ src/icons/muiedit.tsx
@@ -0,0 +1,9 @@
+import { withStyles } from '@material-ui/core';
+import { EditOutlined } from '@material-ui/icons';
+
+export const MuiEditIcon = withStyles({
+	root: {
+        height: '20px',
+        width: '20px',
+    },
+})(EditOutlined);
