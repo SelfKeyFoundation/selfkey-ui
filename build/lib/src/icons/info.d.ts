715a89a9cca4c7ee9372043a592aed3b4b83bb7c
designhorf <designhorf@gmail.com>
2018-12-05 10:17:57 +0100

    build: added missing build files

---
+++ build/lib/src/icons/info.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export default function InfoTooltip(): JSX.Element;
---
+++ build/lib/stories/styleguide/materialui/story-tabs.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export default function TabStories(): JSX.Element;
