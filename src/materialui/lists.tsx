e4ab3150786bdb821179999bac8cf8f5a6f2221c
designhorf <designhorf@gmail.com>
2018-12-13 10:57:26 +0100

    check lists: changed style of icons

--- src/materialui/lists.tsx
+++ src/materialui/lists.tsx
@@ -13,17 +13,16 @@ const defaultIconStyle = {
     width: '16px',
 };
 
-const checkedStyle = {
-    height: '28px',
-    marginRight: '13px',
-    width: '30px',
+const lineStyle = {
+    backgroundColor: '#364357',
+    height: '16px',
+    // marginTop: '28px',
+    width: '2px',
 };
 
-const checkedStyle2 = {
-    height: '44px',
-    marginRight: '13px',
-    width: '30px',
-};
+export const Line = (() => (
+    <span style={ lineStyle }></span>
+));
 
 export const DefaultBullet = (() => (
     <SvgIcon viewBox="0 0 8 8" style={ defaultBulletStyle }>
@@ -110,31 +109,33 @@ export const HourGlassIcon = (() => (
     </SvgIcon>
 ));
 
-export const CheckedIcon = (() => (
-    <SvgIcon viewBox="0 0 30 28" style={ checkedStyle }>
-        <title>checked</title>
+
+const InsideTick = ((color: any) => (
+        <path d="M14.1607102,15.6128249 L18.7094828,11.0640523 C19.0983027,10.6752324 19.7273633,10.6738915 20.1206102,11.0671384 C20.5111345,11.4576627 20.5170209,12.0849413 20.1236963,12.4782659 L14.713208,17.8887542 C14.3243881,18.2775741 13.6953275,18.2789151 13.3020806,17.8856681 C13.2083814,17.7919689 13.1368248,17.6846405 13.087612,17.5701843 L11.1048627,15.5874349 C10.7144135,15.1969858 10.7192331,14.5591232 11.1022757,14.1760806 L11.2087372,14.0696191 C11.5977575,13.6805988 12.2268759,13.6789905 12.6200915,14.0722061 L14.1607102,15.6128249 Z" id="InsideTick" fill={color.color}></path>
+));
+
+const checkedStyle = {
+    height: '44px',
+    marginRight: '13px',
+    marginTop: '-10px',
+    paddingTop: '17px',
+    width: '30px',
+};
+
+export const CheckedIcon = ((item: any ) => (
+    <SvgIcon width="30px" height="44px" viewBox="0 0 30 44" style={ checkedStyle }>
+        <title>item</title>
         <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-            <g id="checked">
-                <rect id="Base" stroke="#495B70" fill="#3B4B59" x="0.5" y="0.5" width="29" height="27" rx="13.5"></rect>
-                <path d="M14.1607102,15.6128249 L18.7094828,11.0640523 C19.0983027,10.6752324 19.7273633,10.6738915 20.1206102,11.0671384 C20.5111345,11.4576627 20.5170209,12.0849413 20.1236963,12.4782659 L14.713208,17.8887542 C14.3243881,18.2775741 13.6953275,18.2789151 13.3020806,17.8856681 C13.2083814,17.7919689 13.1368248,17.6846405 13.087612,17.5701843 L11.1048627,15.5874349 C10.7144135,15.1969858 10.7192331,14.5591232 11.1022757,14.1760806 L11.2087372,14.0696191 C11.5977575,13.6805988 12.2268759,13.6789905 12.6200915,14.0722061 L14.1607102,15.6128249 Z" id="Tick" fill="#1CBA7D"></path>
+            <g id="Group">
+                <g id="list-count/checked">
+                    <g>
+                        <rect id="Base" stroke="#495B70" fill="#3B4B59" x="0.5" y="0.5" width="29" height="27" rx="13.5"></rect>
+                        { item.item !== 'verified' ? <InsideTick color="#1CBA7D" /> : <InsideTick color="#93B0C1" /> }
+                    </g>
+                </g>
+                <polygon id="Line" fill="#364357" opacity="0.800000012" points="15 28 16 28 16 44 15 44"></polygon>
             </g>
         </g>
     </SvgIcon>
 ));
 
-export const CheckedIcon2 = (() => (
-    <SvgIcon width="30px" height="44px" viewBox="0 0 30 44" style={ checkedStyle2 }>
-        <title>Group</title>
-        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g id="Group">
-                <g id="list-count/checked">
-                    <g>
-                        <rect id="Base" stroke="#495B70" fill="#3B4B59" x="0.5" y="0.5" width="29" height="27" rx="13.5"></rect>
-                        <path d="M14.1607102,15.6128249 L18.7094828,11.0640523 C19.0983027,10.6752324 19.7273633,10.6738915 20.1206102,11.0671384 C20.5111345,11.4576627 20.5170209,12.0849413 20.1236963,12.4782659 L14.713208,17.8887542 C14.3243881,18.2775741 13.6953275,18.2789151 13.3020806,17.8856681 C13.2083814,17.7919689 13.1368248,17.6846405 13.087612,17.5701843 L11.1048627,15.5874349 C10.7144135,15.1969858 10.7192331,14.5591232 11.1022757,14.1760806 L11.2087372,14.0696191 C11.5977575,13.6805988 12.2268759,13.6789905 12.6200915,14.0722061 L14.1607102,15.6128249 Z" id="Tick" fill="#1CBA7D"></path>
-                    </g>
-                </g>
-                <polygon id="Line" fill="#364357" opacity="0.300000012" points="15 28 16 28 16 44 15 44"></polygon>
-            </g>
-        </g>
-    </SvgIcon>
-));
