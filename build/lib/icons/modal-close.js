d752e076600171aef2ab12a54ec4d28f7ec00c72
designhorf <designhorf@gmail.com>
2018-12-18 11:23:55 +0100

    build: added modal-close

--- build/lib/icons/modal-close.js
+++ build/lib/icons/modal-close.js
@@ -4,11 +4,11 @@ var React = require("react");
 function ModalCloseIcon() {
     return (React.createElement("div", { style: { height: '38px', marginLeft: '779px', marginTop: '-20px', position: 'absolute', width: '38px', cursor: 'pointer' } },
         React.createElement("svg", { viewBox: "0 0 38 38" },
-            React.createElement("g", { id: "Symbols", stroke: "none", "stroke-width": "1", fill: "none", "fill-rule": "evenodd" },
+            React.createElement("g", { id: "Symbols", stroke: "none", strokeWidth: "1", fill: "none", fillRule: "evenodd" },
                 React.createElement("g", { id: "Close-button", transform: "translate(1.000000, 1.000000)" },
                     React.createElement("g", { id: "icon-close-modal" },
                         React.createElement("circle", { id: "Oval-2", stroke: "#1D505F", fill: "#262F39", cx: "18", cy: "18", r: "18" }),
-                        React.createElement("g", { transform: "translate(9.000000, 9.000000)", fill: "#23E6FE", "fill-rule": "nonzero", id: "Shape" },
+                        React.createElement("g", { transform: "translate(9.000000, 9.000000)", fill: "#23E6FE", fillRule: "nonzero", id: "Shape" },
                             React.createElement("polygon", { points: "1.81534091 0.639204545 0.639204545 1.81534091 7.82386364 9 0.639204545 16.1846591 1.81534091 17.3607955 9 10.1761364 16.1846591 17.3607955 17.3607955 16.1846591 10.1761364 9 17.3607955 1.81534091 16.1846591 0.639204545 9 7.82386364" }))))))));
 }
 exports.default = ModalCloseIcon;
