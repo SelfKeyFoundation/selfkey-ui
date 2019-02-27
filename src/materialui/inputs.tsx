f23b51d4ef8e8499fa65cc8c180baa9a3ef119fc
Maxim Kovalov <max.kovalov@gmail.com>
2019-02-27 21:26:27 +0200

    fix(file-widget): styles and native input

--- src/materialui/inputs.tsx
+++ src/materialui/inputs.tsx
@@ -89,7 +89,7 @@ export const FileUploadWidget = injectSheet(fileUploadStyles)<FileUploadWidgetPr
 						<Grid container direction="row" justify="space-between" alignItems="center">
 							<Grid item>
 								<Typography color="secondary" variant="caption">
-									Test
+									{props.placeholder} || 'Please upload a document'
 								</Typography>
 							</Grid>
 							<Grid item>
