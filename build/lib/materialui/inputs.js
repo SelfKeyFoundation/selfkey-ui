6cf3ae5aac05d9a176eb49b6d8cabfa249994124
Maxim Kovalov <max.kovalov@gmail.com>
2019-02-27 18:06:13 +0200

    feat(file-upload): add single file upload widget

--- build/lib/materialui/inputs.js
+++ build/lib/materialui/inputs.js
@@ -1,6 +1,30 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
 var core_1 = require("@material-ui/core");
+var react_jss_1 = require("react-jss");
+var delete_1 = require("../icons/delete");
+var file_default_1 = require("../icons/file-default");
 var colors_1 = require("../colors");
 exports.FileUploadLabel = core_1.withStyles({
     root: {
@@ -27,4 +51,64 @@ exports.DecimalInput = core_1.withStyles({
         textAlign: 'right',
     },
 })(core_1.Input);
+var fileUploadStyles = {
+    form: {
+        width: '100%',
+        height: '46px',
+        backgroundColor: colors_1.baseDark,
+        border: '1px solid #384656',
+        paddingLeft: '15px',
+    },
+    button: {
+        width: '100%',
+    },
+    fileItem: {
+        padding: '0 20px',
+    },
+};
+exports.FileUploadWidget = react_jss_1.default(fileUploadStyles)(function (_a) {
+    var classes = _a.classes, id = _a.id, file = _a.file, onClearForm = _a.onClearForm, onChange = _a.onChange, onBlur = _a.onBlur, onFocus = _a.onFocus, props = __rest(_a, ["classes", "id", "file", "onClearForm", "onChange", "onBlur", "onFocus"]);
+    var eventHandlers = {};
+    if (onChange) {
+        eventHandlers.onChange = function (evt) {
+            onChange(evt);
+        };
+    }
+    if (onBlur) {
+        eventHandlers.onBlur = function (evt) {
+            onBlur(evt);
+        };
+    }
+    if (onFocus) {
+        eventHandlers.onFocus = function (evt) {
+            onFocus(evt);
+        };
+    }
+    if (!onClearForm) {
+        onClearForm = function () { };
+    }
+    id = id || 'key-upload';
+    return (React.createElement(core_1.Grid, { container: true, direction: "column", spacing: 24 },
+        React.createElement(core_1.Grid, { item: true },
+            React.createElement("div", { className: classes.form },
+                React.createElement(core_1.Grid, { container: true, direction: "row", justify: "space-between", alignItems: "center" },
+                    React.createElement(core_1.Grid, { item: true },
+                        React.createElement(core_1.Typography, { color: "secondary", variant: "caption" }, "Test")),
+                    React.createElement(core_1.Grid, { item: true },
+                        React.createElement(core_1.Button, { variant: "contained", size: "large", component: "label" },
+                            "Upload",
+                            React.createElement(exports.FileUploadInput, __assign({ id: id, type: "file" }, props, eventHandlers))))))),
+        file ? (React.createElement(core_1.Grid, { item: true },
+            React.createElement(core_1.Grid, { container: true, direction: "row", justify: "space-between", alignItems: "center", className: classes.fileItem },
+                React.createElement(core_1.Grid, { item: true },
+                    React.createElement(core_1.Grid, { container: true, direction: "row", alignItems: "center", spacing: 16 },
+                        React.createElement(core_1.Grid, { item: true },
+                            React.createElement(file_default_1.FileDefaultIcon, null)),
+                        React.createElement(core_1.Grid, { item: true },
+                            React.createElement("a", { href: file.url },
+                                React.createElement(core_1.Typography, { variant: "body2" }, file.name))))),
+                React.createElement(core_1.Grid, { item: true },
+                    React.createElement(core_1.IconButton, { onClick: onClearForm },
+                        React.createElement(delete_1.DeleteIcon, null)))))) : null));
+});
 //# sourceMappingURL=inputs.js.map
--- src/materialui/inputs.tsx
+++ src/materialui/inputs.tsx
@@ -1,35 +1,130 @@
-
-import { 
-    withStyles, 
-    Input, 
-    InputLabel 
-} from '@material-ui/core';
-import { primary } from '../colors';
+import * as React from 'react';
+import { withStyles, Input, InputLabel, Typography, Grid, Button, IconButton } from '@material-ui/core';
+import injectSheet, { StyleSheet } from 'react-jss';
+import { DeleteIcon } from '../icons/delete';
+import { FileDefaultIcon } from '../icons/file-default';
+import { primary, baseDark } from '../colors';
 
 export const FileUploadLabel = withStyles({
 	root: {
-        alignItems: 'center',
-        cursor: 'pointer',
-        display: 'flex',
-        flexDirection: 'column',
-        height: '120px',
-        justifyContent: 'space-between',
-        marginTop: '100px',
-        textAlign: 'center',
-        '&:hover': {
-            color: primary,
-        },
-    },
+		alignItems: 'center',
+		cursor: 'pointer',
+		display: 'flex',
+		flexDirection: 'column',
+		height: '120px',
+		justifyContent: 'space-between',
+		marginTop: '100px',
+		textAlign: 'center',
+		'&:hover': {
+			color: primary,
+		},
+	},
 })(InputLabel);
 
 export const FileUploadInput = withStyles({
 	root: {
-        display: 'none',
-    },
+		display: 'none',
+	},
 })(Input);
 
 export const DecimalInput = withStyles({
-    inputType: {
-        textAlign: 'right',
-    },
+	inputType: {
+		textAlign: 'right',
+	},
 })(Input);
+
+export type FileUploadWidgetProps = any;
+
+const fileUploadStyles: StyleSheet = {
+	form: {
+		width: '100%',
+		height: '46px',
+		backgroundColor: baseDark,
+		border: '1px solid #384656',
+		paddingLeft: '15px',
+	},
+	button: {
+		width: '100%',
+	},
+	fileItem: {
+		padding: '0 20px',
+	},
+};
+
+export const FileUploadWidget = injectSheet(fileUploadStyles)<FileUploadWidgetProps>(
+	({ classes, id, file, onClearForm, onChange, onBlur, onFocus, ...props }) => {
+		const eventHandlers: any = {};
+		if (onChange) {
+			eventHandlers.onChange = (evt: any) => {
+				onChange(evt);
+			};
+		}
+		if (onBlur) {
+			eventHandlers.onBlur = (evt: any) => {
+				onBlur(evt);
+			};
+		}
+		if (onFocus) {
+			eventHandlers.onFocus = (evt: any) => {
+				onFocus(evt);
+			};
+		}
+
+		if (!onClearForm) {
+			onClearForm = () => {};
+		}
+
+		id = id || 'key-upload';
+
+		return (
+			<Grid container direction="column" spacing={24}>
+				<Grid item>
+					<div className={classes.form}>
+						<Grid container direction="row" justify="space-between" alignItems="center">
+							<Grid item>
+								<Typography color="secondary" variant="caption">
+									Test
+								</Typography>
+							</Grid>
+							<Grid item>
+								<Button variant="contained" size="large" component="label">
+									Upload
+									<FileUploadInput id={id} type="file" {...props} {...eventHandlers} />
+								</Button>
+							</Grid>
+						</Grid>
+					</div>
+				</Grid>
+				{file ? (
+					<Grid item>
+						<Grid
+							container
+							direction="row"
+							justify="space-between"
+							alignItems="center"
+							className={classes.fileItem}
+						>
+							<Grid item>
+								<Grid container direction="row" alignItems="center" spacing={16}>
+									<Grid item>
+										<FileDefaultIcon />
+									</Grid>
+									<Grid item>
+										<a href={file.url}>
+											<Typography variant="body2">{file.name}</Typography>
+										</a>
+									</Grid>
+								</Grid>
+							</Grid>
+							<Grid item>
+								<IconButton onClick={onClearForm}>
+									<DeleteIcon />
+								</IconButton>
+							</Grid>
+						</Grid>
+					</Grid>
+				) : null}
+			</Grid>
+		);
+	}
+);
