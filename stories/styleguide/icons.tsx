bbea57c1a5599abdb917e220bd1301f7b89f2457
Maxim Kovalov <max.kovalov@gmail.com>
2019-03-14 23:10:41 +0200

    feat(add-icon): add icon

---
+++ stories/styleguide/icons.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { MuiAddIcon } from '../../src/icons/muiadd';
+
+export const IconsStory = () => (
+	<ul>
+		<li>
+			MuiAddIcon <MuiAddIcon />
+		</li>
+	</ul>
+);
--- stories/styleguide/index.stories.tsx
+++ stories/styleguide/index.stories.tsx
@@ -56,6 +56,7 @@ import FileUploadStory from './materialui/story-fileUpload';
 import ModalStories from './materialui/story-modals';
 import DatePickerStories from './materialui/story-datepickers';
 import SliderStory from './materialui/story-sliders';
+import { IconsStory } from './icons';
 
 setup();
 
@@ -78,6 +79,8 @@ const transferModal = host({
 const commonStory = storiesOf('Common', module).addDecorator(lightOnDark) as Story & { addWithJSX: Function };
 
 commonStory.addWithJSX('Slider', () => {});
+
+commonStory.addWithJSX('Icons', () => <IconsStory />);
 
 const tStory = storiesOf('Typography', module).addDecorator(lightOnDark) as Story & { addWithJSX: Function };
 
