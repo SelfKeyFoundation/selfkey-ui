677f3f2bbb494d6c9a68c396c7cbd9f641c151f9
designhorf <designhorf@gmail.com>
2019-04-03 18:22:38 +0200

    fix: typos of svg props

--- build/lib/icons/file-multiple.js
+++ build/lib/icons/file-multiple.js
@@ -21,8 +21,8 @@ var styles = {
     }
 };
 exports.FileMultipleIcon = react_jss_1.default(styles)(function (props) { return (React.createElement(core_1.SvgIcon, __assign({}, props, { viewBox: '0 0 31 39' }),
-    React.createElement("g", { id: "\uD83D\uDCBB-ID-Dashboard-multiple-files", stroke: "none", "stroke-width": "1", fill: "none", "fill-rule": "evenodd" },
-        React.createElement("g", { id: "multiple-files", transform: "translate(-392.000000, -1692.000000)", stroke: "#697C95", "stroke-width": "1.71428571" },
+    React.createElement("g", { id: "\uD83D\uDCBB-ID-Dashboard-multiple-files", stroke: "none", strokeWidth: "1", fill: "none", fillRule: "evenodd" },
+        React.createElement("g", { id: "multiple-files", transform: "translate(-392.000000, -1692.000000)", stroke: "#697C95", strokeWidth: "1.71428571" },
             React.createElement("g", { id: "Group", transform: "translate(394.000000, 1693.000000)" },
                 React.createElement("path", { d: "M5.91075996,6.33834121 L5.91075996,0.238509317 L19.7794574,0.238509317 L19.7794574,8.47453416 L28.0956339,8.47453416 L28.0956339,28.5614907 L22.2177567,28.5614907 L22.2177567,36.6857143 L-0.857142857,36.6857143 L-0.857142857,6.54112651 L5.91075996,6.33834121 Z", id: "icon-file-multiple" }),
                 React.createElement("polyline", { id: "Path-3", points: "5.48849105 6.31304348 13.1649616 6.31304348 22.213555 15.7535156 22.213555 28.9714286" }),
--- src/icons/book.tsx
+++ src/icons/book.tsx
@@ -17,12 +17,12 @@ export const BookIcon = injectSheet(styles)<SvgIconProps>((props: SvgIconProps &
     <desc>Created with Sketch.</desc>
     <defs>
         <linearGradient x1="50%" y1="97.783801%" x2="41.8489583%" y2="-16.3970588%" id="book-linearGradient-1">
-            <stop stop-color="#08BCCD" offset="0%"></stop>
-            <stop stop-color="#2DA1F8" offset="100%"></stop>
+            <stop stopColor="#08BCCD" offset="0%"></stop>
+            <stop stopColor="#2DA1F8" offset="100%"></stop>
         </linearGradient>
     </defs>
-    <g id="💻-ID-Dashboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="403---Identity-Summary" transform="translate(-240.000000, -958.000000)" fill="url(#book-linearGradient-1)" fill-rule="nonzero">
+    <g id="💻-ID-Dashboard" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g id="403---Identity-Summary" transform="translate(-240.000000, -958.000000)" fill="url(#book-linearGradient-1)" fillRule="nonzero">
             <g id="Area-box-2" transform="translate(150.000000, 462.000000)">
                 <g id="Additional" transform="translate(14.000000, 390.000000)">
                     <g id="Explanatory" transform="translate(10.000000, 106.000000)">
