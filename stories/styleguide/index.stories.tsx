bc93bc4587535705e4e368d0b2a1f6e004ec4cb9
designhorf <designhorf@gmail.com>
2019-04-05 08:46:26 +0200

    deleted old Typography and Images stories

--- stories/styleguide/index.stories.tsx
+++ stories/styleguide/index.stories.tsx
@@ -5,9 +5,6 @@ import { host } from 'storybook-host';
 import JSXAddon from 'storybook-addon-jsx';
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
-import { H1 } from '../../src/typography/headings';
-import { P } from '../../src/typography/paragraph';
-import { Logo } from '../../src/images/logo';
 import { CryptoPriceList } from './crypto-price-list';
 import { TokenList } from './token-list';
 import { CryptoChartBox } from '../../src/price/crypto-chart-box';
@@ -85,22 +82,6 @@ commonStory.addWithJSX('Icons', () => (
 		<IconsStory />
 	</SelfkeyDarkTheme>
 ));
-
-const tStory = storiesOf('Typography', module).addDecorator(lightOnDark) as Story & { addWithJSX: Function };
-
-tStory.addWithJSX('Primary Heading (H1)', () => <H1>Primary Heading</H1>);
-
-tStory.addWithJSX('Paragraph', () => (
-	<P>
-		Create a new SelfKey ID to start building your identity profile. If you were previously whitelisted for the KEY
-		token sale, simply import the zip file we provided as your SelfKey ID. This data is stored locally on your
-		device, not in a server or on the blockchain.
-	</P>
-));
-
-const iStory = storiesOf('Images', module).addDecorator(lightOnDark) as Story & { addWithJSX: Function };
-
-iStory.addWithJSX('SelfKey Logo', () => <Logo />);
 
 const priceStory = storiesOf('Prices', module).addDecorator(lightOnDark) as Story & { addWithJSX: Function };
 
