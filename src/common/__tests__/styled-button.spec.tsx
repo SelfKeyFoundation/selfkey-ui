0e12fb0282419f58a893d60c448a294c6c2d09e9
designhorf <designhorf@gmail.com>
2019-04-09 17:50:13 +0200

    added new tests

---
+++ src/common/__tests__/styled-button.spec.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { create } from 'react-test-renderer';
+import StyledButton from '../styled-button';
+
+
+describe('Styled Button', () => {
+  it('should show Styled Button', () => {
+    const component = create(
+        <StyledButton color="primary" />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+
+
+
