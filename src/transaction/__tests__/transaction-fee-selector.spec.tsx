a49d8a2a9428c08ca98201974e4d92270e681657
designhorf <designhorf@gmail.com>
2019-04-10 20:33:55 +0200

    test for transaction fee selector

---
+++ src/transaction/__tests__/transaction-fee-selector.spec.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { create } from 'react-test-renderer';
+import { TransactionFeeSelector } from '../transaction-fee-selector';
+
+describe('Transaction Fee Selector', () => {
+  it('should show Transaction Fee Selector screen', () => {
+    const component = create(
+      <TransactionFeeSelector 
+        minGasPrice={1}
+        maxGasPrice={2}
+        gasLimit={2}
+        fiat="USD"
+        fiatRate={1}
+        defaultValue={0}
+        value={0}
+        onChange={(value: number) => console.log(value)}
+      />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+
+
+
