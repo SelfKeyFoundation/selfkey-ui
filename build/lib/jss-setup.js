581f07a6dddc681c4a740a73d2452abdd2e37ef2
designhorf <designhorf@gmail.com>
2019-05-07 08:19:40 +0200

    changed body bg

--- build/lib/jss-setup.js
+++ build/lib/jss-setup.js
@@ -9,6 +9,7 @@ exports.setup = function () {
     var styles = {
         '@global': {
             body: {
+                background: 'linear-gradient(135deg, rgba(43,53,64,1) 0%, rgba(30,38,46,1) 100%)',
                 fontFamily: 'Lato, arial, sans-serif',
                 fontSmoothing: 'antialiased',
                 osxFontSmoothing: 'grayscale',
