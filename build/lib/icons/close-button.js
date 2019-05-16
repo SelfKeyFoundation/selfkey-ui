13625aa3d7f24f7182a7983b9c2cff47b0687000
designhorf <designhorf@gmail.com>
2019-05-16 07:22:24 +0200

    hover state to Close Button icon

--- build/lib/icons/close-button.js
+++ build/lib/icons/close-button.js
@@ -17,7 +17,17 @@ var core_1 = require("@material-ui/core");
 var styles = {
     root: {
         width: '38px !important',
-        height: '38px !important'
+        height: '38px !important',
+        transition: 'all 0.2s ease-out',
+        cursor: 'pointer',
+        '& circle': {
+            stroke: '#1D505F',
+        },
+        '&:hover': {
+            '& circle': {
+                stroke: '#23E6FE',
+            }
+        }
     }
 };
 exports.CloseButtonIcon = react_jss_1.default(styles)(function (props) { return (React.createElement(core_1.SvgIcon, __assign({}, props, { viewBox: '0 0 38 38' }),
@@ -25,7 +35,7 @@ exports.CloseButtonIcon = react_jss_1.default(styles)(function (props) { return 
     React.createElement("g", { id: "Symbols", stroke: "none", strokeWidth: "1", fill: "none", fillRule: "evenodd" },
         React.createElement("g", { id: "Close-button", transform: "translate(1.000000, 1.000000)" },
             React.createElement("g", { id: "Close" },
-                React.createElement("circle", { id: "Oval-2", stroke: "#1D505F", fill: "#262F39", cx: "18", cy: "18", r: "18" }),
+                React.createElement("circle", { id: "Oval-2", fill: "#262F39", cx: "18", cy: "18", r: "18" }),
                 React.createElement("g", { id: "icons8-delete_sign", transform: "translate(9.000000, 9.000000)", fill: "#23E6FE", fillRule: "nonzero" },
                     React.createElement("polygon", { id: "Shape", points: "1.81534091 0.639204545 0.639204545 1.81534091 7.82386364 9 0.639204545 16.1846591 1.81534091 17.3607955 9 10.1761364 16.1846591 17.3607955 17.3607955 16.1846591 10.1761364 9 17.3607955 1.81534091 16.1846591 0.639204545 9 7.82386364" }))))))); });
 exports.default = exports.CloseButtonIcon;
--- src/icons/close-button.tsx
+++ src/icons/close-button.tsx
@@ -7,7 +7,17 @@ import { SvgIconProps } from '@material-ui/core/SvgIcon';
 const styles: StyleSheet = {
   root: {
     width: '38px !important',
-    height: '38px !important'
+    height: '38px !important',
+    transition: 'all 0.2s ease-out',
+    cursor: 'pointer',
+    '& circle': {
+      stroke: '#1D505F',
+    },
+    '&:hover': {
+      '& circle': {
+        stroke: '#23E6FE',
+      } 
+    }
   }
 };
 
@@ -17,7 +27,7 @@ export const CloseButtonIcon = injectSheet(styles)<SvgIconProps>((props: SvgIcon
     <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="Close-button" transform="translate(1.000000, 1.000000)">
             <g id="Close">
-                <circle id="Oval-2" stroke="#1D505F" fill="#262F39" cx="18" cy="18" r="18"></circle>
+                <circle id="Oval-2" fill="#262F39" cx="18" cy="18" r="18"></circle>
                 <g id="icons8-delete_sign" transform="translate(9.000000, 9.000000)" fill="#23E6FE" fillRule="nonzero">
                     <polygon id="Shape" points="1.81534091 0.639204545 0.639204545 1.81534091 7.82386364 9 0.639204545 16.1846591 1.81534091 17.3607955 9 10.1761364 16.1846591 17.3607955 17.3607955 16.1846591 10.1761364 9 17.3607955 1.81534091 16.1846591 0.639204545 9 7.82386364"></polygon>
                 </g>
@@ -28,7 +38,3 @@ export const CloseButtonIcon = injectSheet(styles)<SvgIconProps>((props: SvgIcon
 ));
 
 export default CloseButtonIcon;
-
-
-
-
