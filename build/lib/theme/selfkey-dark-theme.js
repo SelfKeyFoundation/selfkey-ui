32e514de75ae13a114f4d1863036013e643d53e0
designhorf <designhorf@gmail.com>
2019-07-10 06:09:40 +0200

    modal bg opacity changed

--- build/lib/theme/selfkey-dark-theme.js
+++ build/lib/theme/selfkey-dark-theme.js
@@ -822,8 +822,8 @@ var theme = core_1.createMuiTheme({
         },
         MuiBackdrop: {
             root: {
-                // backgroundColor: base,
                 background: 'linear-gradient(135deg, rgba(43,53,64,1) 0%, rgba(30,38,46,1) 100%)',
+                opacity: '0.7 !important'
             },
             invisible: {
                 background: 'transparent',
