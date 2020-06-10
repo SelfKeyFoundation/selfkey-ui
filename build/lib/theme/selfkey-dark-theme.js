759e20edb35e3240d6e3ebc8c399b00d9b58c2b9
Andre Goncalves <andre@andregoncalves.com>
2020-06-10 13:10:29 +0100

    feat: export main theme

--- build/lib/theme/selfkey-dark-theme.js
+++ build/lib/theme/selfkey-dark-theme.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 import { primary, typography, error, primaryTint, warning, white, grey, base, baseDark, baseLight } from '../colors';
-const theme = createMuiTheme({
+export const theme = createMuiTheme({
     // Reset and Globals
     '@global': {
         'html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video': {
