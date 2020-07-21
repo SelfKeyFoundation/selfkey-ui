105b786852f743a1cc0246fc94fec832ccaf6bb3
Ferenc Horvath <designhorf@gmail.com>
2020-07-21 12:22:23 +0200

    created Affiliate integration icon

---
+++ build/lib/icons/affiliate-integration.js
@@ -0,0 +1,35 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AffiliateIntegrationIcon = void 0;
+var React = require("react");
+var core_1 = require("@material-ui/core");
+var core_2 = require("@material-ui/core");
+var styles = core_1.createStyles({});
+exports.AffiliateIntegrationIcon = core_1.withStyles(styles)(function (props) { return (React.createElement(core_2.SvgIcon, __assign({ style: { width: props.width || '21px', height: props.height || '25px' } }, props, { viewBox: props.viewBox || "0 0 21 25" }),
+    React.createElement("defs", null,
+        React.createElement("linearGradient", { x1: "15.108125%", y1: "0.550063776%", x2: "84.6506684%", y2: "99.1080901%", id: "linearGradient-affiliate" },
+            React.createElement("stop", { "stop-color": "#4FC1E9", offset: "0%" }),
+            React.createElement("stop", { "stop-color": "#0B9BD0", offset: "100%" }))),
+    React.createElement("g", { id: "\uD83C\uDF08-UI-Guidelines", stroke: "none", "stroke-width": "1", fill: "none", "fill-rule": "evenodd" },
+        React.createElement("g", { id: "6.-Style-Guide---UI-Elements-2", transform: "translate(-1343.000000, -679.000000)" },
+            React.createElement("g", { id: "Badges", transform: "translate(1047.000000, 679.000000)" },
+                React.createElement("g", { id: "vendor-tag", transform: "translate(296.000000, 0.000000)" },
+                    React.createElement("g", { id: "aff" },
+                        React.createElement("g", null,
+                            React.createElement("g", { id: "affiliate" },
+                                React.createElement("path", { d: "M4,0 L21,0 L21,0 L21,25 L4,25 C1.790861,25 2.705415e-16,23.209139 0,21 L0,4 C-2.705415e-16,1.790861 1.790861,4.05812251e-16 4,0 Z", id: "sign-background", fill: "url(#linearGradient-affiliate)" }),
+                                React.createElement("g", { id: "icon-affiliate", transform: "translate(6.000000, 8.000000)", fill: "#FFFFFF" },
+                                    React.createElement("polygon", { id: "Star", points: "4.5 6.75 1.85496636 8.14057647 2.36012284 5.19528824 0.220245677 3.10942353 3.17748318 2.67971176 4.5 0 5.82251682 2.67971176 8.77975432 3.10942353 6.63987716 5.19528824 7.14503364 8.14057647" }))))))))))); });
+exports.default = exports.AffiliateIntegrationIcon;
+//# sourceMappingURL=affiliate-integration.js.map
