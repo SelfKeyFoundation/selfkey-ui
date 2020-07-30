06756ffa96df3103ab1afa668cba19a249890f50
Ferenc Horvath <designhorf@gmail.com>
2020-07-30 10:39:48 +0200

    feat: changed style and added extra props

--- build-esnext/lib/materialui/button.js
+++ build-esnext/lib/materialui/button.js
@@ -5,10 +5,15 @@ const styles = createStyles({
     bold: {
         fontWeight: 600
     },
+    button: {
+        height: '36px',
+        minWidth: '80px',
+        width: '80px'
+    }
 });
-export const BackButton = withStyles(styles)(({ classes, onclick, leftPosition, position, ...props }) => {
+export const BackButton = withStyles(styles)(({ classes, id, size, onclick, leftPosition, position, ...props }) => {
     return (React.createElement("div", { style: { left: leftPosition || 'initial', position: position || 'initial' } },
-        React.createElement(Button, { variant: "outlined", color: "secondary", size: "small", onClick: onclick },
+        React.createElement(Button, { variant: "outlined", color: "secondary", size: size ? size : 'null', onClick: onclick, id: id, className: classes.button },
             React.createElement(Typography, { variant: "subtitle2", color: "secondary", className: classes.bold }, "\u2039 Back"))));
 });
 //# sourceMappingURL=button.js.map
