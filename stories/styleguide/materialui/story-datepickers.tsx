d45a02e7dec22b62a8d32674467f281121dd7b2d
Maxim Kovalov <max.kovalov@gmail.com>
2020-10-05 17:00:34 +0300

    feat(datepicker): add disabled dates

--- stories/styleguide/materialui/story-datepickers.tsx
+++ stories/styleguide/materialui/story-datepickers.tsx
@@ -1,31 +1,42 @@
 import * as React from 'react';
 import { Typography } from '@material-ui/core';
+import * as moment from 'moment';
 import KeyPicker from '../../../src/materialui/datepicker';
 
 const underlineStyle = {
-    textDecoration: 'underline',
+	textDecoration: 'underline'
 };
 
 export default function DatePickerStories() {
-    return (
-        <div>
-            <Typography variant="h3" style={underlineStyle} gutterBottom>
-                Date Time Picker
-            </Typography>
+	const today = moment(Date.now()).add(1, 'month');
+	return (
+		<div>
+			<Typography variant="h3" style={underlineStyle} gutterBottom>
+				Date Time Picker
+			</Typography>
 
-            <KeyPicker
-                includeTime
-                onChange={(data: any) => console.log(data)}
-                onFocus={(evt: any) => console.log(evt)}
-                onBlur={(evt: any) => console.log(evt)}
-            />
+			<KeyPicker
+				includeTime
+				onChange={(data: any) => console.log(data)}
+				onFocus={(evt: any) => console.log(evt)}
+				onBlur={(evt: any) => console.log(evt)}
+			/>
 
-            <Typography variant="h3" style={underlineStyle} gutterBottom>
-                Date Picker
-            </Typography>
+			<Typography variant="h3" style={underlineStyle} gutterBottom>
+				Date Picker
+			</Typography>
 
-            <KeyPicker />
-            <p style={{ marginBottom: '400px' }}>&nbsp;</p>
-        </div>
-    );
+			<KeyPicker />
+			<Typography variant="h3" style={underlineStyle} gutterBottom>
+				Date Picker: Allow only events in one month
+			</Typography>
+			<KeyPicker
+				isValidDate={(current: any): boolean => {
+					return current.isSameOrAfter(today, 'day');
+				}}
+				viewDate={today}
+			/>
+			<p style={{ marginBottom: '400px' }}>&nbsp;</p>
+		</div>
+	);
 }
